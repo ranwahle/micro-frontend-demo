00acf46e59301e5b3281a3e06e60b763bd49ec48
Ran <ran.wahle@gmail.com>
2024-01-11 11:40:35 +0200

    Code clueanup

--- applications/shell/src/Navbar/Navbar.tsx
+++ applications/shell/src/Navbar/Navbar.tsx
@@ -1,29 +1,8 @@
 import { useGetRoutes } from "../hooks/useGetRoutes.tsx";
 import {StyledLink, StyledNav} from "./Navbar.styles.tsx";
 
-
-// const menuItems = [
-//     {
-//         label: 'Home',
-//         path: '/',
-//     },
-//     {
-//         label: 'Washing macine Status',
-//         path: '/app/washingmachine',
-//     },
-//     {
-//         label: 'Clothes In Stock',
-//         path: '/app/washingmachine/clothes-In-Stock',
-//     },
-//     {
-//         label: 'Clothes On hanger',
-//         path: '/app/hanger',
-//     },
-// ]
-
 export function Navbar() {
     const routes = useGetRoutes();
-    console.log(routes);
     return <StyledNav>
         <StyledLink to="/">Home</StyledLink>
         {routes.map(item => <div key={item.url}>
